2d4dbd786156038e10af2f262201283122f6cc30
danielkarlsson2 <daniel@dkmusic.se>
2022-02-16 15:10:49 +0100

    lektion 4

--- Lektion-4/1-websocket-chat/index.js
+++ Lektion-4/1-websocket-chat/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socket = require('socket.io');
+const { userConnect, userDisconnect } = require('./public/users');
+
+const { userConnect } = require('/users')
+
 
 const app = express();
 const server = http.createServer(app);
@@ -18,5 +22,22 @@ const io = socket(server)
 io.on('connection', socket => {
     // console.log('New user connected ', socket.id)
 
-    socket.emit('message', 'Welcome to the chat!')
+    socket.on('user', userName => {
+        userConnect(socket.id, userName)
+        socket.broadcast.emit('user', `${userName} has joined the chat`)
+    })
+
+
+    socket.on('message', data => {
+        io.sockets.emit('message', data)
+    })
+
+    socket.on('typing', data=> {
+        socket.broadcast.emit('typing', data)
+    })
+
+    socket.on('disconnect', () => {
+        const user = userDisconnect(socket.id) 
+        io.emit('user', `${user.userName} has left the chat`);
+    })
 })
--- Lektion-4/1-websocket-chat/public/script.js
+++ Lektion-4/1-websocket-chat/public/script.js
@@ -1,5 +1,69 @@
 const socket = io();
 
+const messages = document.querySelector('.messages')
+const chatForm = document.querySelector('#chatForm')
+const chatMessage = document.querySelector('#chatMessage')
+const chatWindow = document.querySelector('.chat-window')
+const feedback = document.querySelector('#feedback')
+
+
+const userName = new URLSearchParams(window.location.search).get('name');
+document.querySelector('#me').innerText = userName;
+
+socket.on('connect', () => {
+    socket.emit('user', userName);
+})
+
+socket.on('user', data => {
+    messages.innerHTML += `<p class="inline-feedback"></p>`
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+})
+
+
+
 socket.on('message', data => {
-    console.log(data)
+    let position = data.id === socket.id ? 'right' : ''
+
+
+    // feedback.insertAdjacentHTML(` 
+    //  <div class="single-message ${position}">
+    //      <p class="single-message_name">${data.name}</p>
+    //      <p class="single-message_msg">${data.message}</p>
+    //  </div>`)
+    messages.innerHTML += ` 
+    <div class="single-message ${position}">
+        <p class="single-message_name">${data.name}</p>
+        <p class="single-message_msg">${data.message}</p>
+    </div>`
+
+    feedback.innerText = ''
+    feedback.classList.add('d-none')
+
+    // console.log(chatWindow.scrollHeight);
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+})
+
+socket.on('typing', data => {
+    feedback.innerText = `${data} is typing a message...`
+    feedback.classList.remove('d-none')
+})
+
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if(chatMessage.value.trim() !== '') {
+        socket.emit('message', {
+            id: socket.id,
+            message: chatMessage.value,
+            name: userName
+        })
+    }
+
+    chatMessage.value = '';
+    chatMessage.focus();
+
+})
+
+chatMessage.addEventListener('keypress', () => {
+    socket.emit('typing', userName)
 })
