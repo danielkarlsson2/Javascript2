0a06cb8b2a49ad4f9771b2c05cfb5142fc13d6b6
danielkarlsson2 <daniel@dkmusic.se>
2022-02-16 10:48:47 +0100

    Lektion 4

--- Lektion-3(new)/backend/app.js
+++ Lektion-3(new)/backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const todosController = require('./controllers/todoController');
 
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
