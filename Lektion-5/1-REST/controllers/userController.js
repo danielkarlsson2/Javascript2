4f44348266d02f5e7484e73b78ba909eac292e3f
danielkarlsson2 <daniel@dkmusic.se>
2022-02-21 14:46:12 +0100

    Lektion 5

--- Lektion-5/1-REST/controllers/userController.js
+++ Lektion-5/1-REST/controllers/userController.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const userModel = require('../models/users/userModel');
 
 // Register a new user
-// router.post('/register')
+router.post('/register', userModel.registerUser);
 
 // Login a user
-// router.post('/login')
+router.post('/login', userModel.loginUserWithEmailAndPassword);
 
 module.exports = router;
