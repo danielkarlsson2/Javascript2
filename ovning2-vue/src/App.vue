0b2ddc283d98a24310f30fcd3dc54734bf21291b
danielkarlsson2 <daniel@dkmusic.se>
2022-02-24 13:47:35 +0100

    Övning2

--- ovning2-vue/src/App.vue
+++ ovning2-vue/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <Navbar />
+
+  <HeroSection />
+
+  <FooterComponent />
 </template>
 
 <script>
+
+import FooterComponent from './components/FooterComponent.vue'
+import HeroSection from './components/HeroSection.vue'
 import Navbar from './components/Navbar.vue'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, HeroSection, FooterComponent},
 
 }
 </script>
--- ovning2-vue/src/components/HeroSection.vue
+++ ovning2-vue/src/components/HeroSection.vue
@@ -1,13 +1,47 @@
 <template>
-  
+  <!-- Background image -->
+  <div
+    class="p-5 text-center bg-image"
+    style="background-image: url('https://www.filmtopp.se/media/2015/06/t2-min.jpg?w=810&fit=max&s=b3ec51eddaf0e7ac6a7de7b7f82b9031');"
+  >
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white img-section align-items-baseline">
+          <!-- <h1 class="mb-3">Heading</h1> -->
+          <!-- <h4 class="mb-3">Subheading</h4> -->
+          <a class="btn btn-outline-light btn-lg knapp" href="#!" role="button" @click="clickFunction"
+          >To the choper</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+    name: 'HeroSection',
+    methods: {
+      clickFunction() {
+        console.log('Hasta la vista baby')
+      }
+    }
 }
 </script>
 
 <style>
-
+.bg-image {
+    height: 550px;
+    background-color: black;
+    background-size: contain;
+    top: 0;
+}
+/* .img-section {
+    
+} */
+.knapp:hover {
+    transform: scale(1.2);
+    background-color: white;
+    color: black;
+}
 </style>
