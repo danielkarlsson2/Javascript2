bc33761010e219923d1784f21721f9359055318f
danielkarlsson2 <daniel@dkmusic.se>
2022-03-09 10:01:56 +0100

    Lektion 10

---
+++ Lektion-10/1-slots/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <h1>Modals</h1>
+    <button @click="showModal = true">Show modal</button>
+    <button @click="showModal2 = true">Show modal2</button>
+    <button @click="showModal3 = true">Show modal3</button>
+
+    <!-- <Modal v-if="showModal" @close="showModal = false"/> -->
+    <Modal v-if="showModal">
+      <h2>Buy this product now!</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, aliquam.</p>
+      <button @click="showModal = false">Close</button>
+      </Modal>
+      <Modal v-if="showModal2"> 
+        <template v-slot:links>
+          <a href="#">My website</a>
+          <a href="#">My Yahoo</a>
+          <a href="#">Nån annanstans</a>
+        </template>
+        <form>
+          <div class="input-group">            
+            <label>
+              <span>Email:</span>
+              <input type="email">
+            </label>
+          </div>
+          <div class="input-group">            
+            <label>
+              <span>Password:</span>
+              <input type="email">
+            </label>
+          </div>
+          <button @click="showModal2 = false">Close</button>
+        </form>
+      </Modal>
+      <Modal v-if="showModal3" @close="showModal3 = false" />
+    
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+
+  },
+  data() {
+    return { 
+      showModal: false,
+      showModal2: false,
+      showModal3: false
+    }
+  }
+}
+</script>
+
+<style>
+#app, #modals{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button {
+  background: orangered;
+  color: #fff;
+  padding: .5em 1.2em;
+  border: 0;
+  border-radius: .5em;
+  cursor: pointer;
+  margin: 1rem;
+}
+</style>
---
+++ Lektion-10/1-slots/src/components/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+<teleport to="#modals">
+  <div class="modal-wrapper">
+      <div class="modal">
+          <slot>
+            <h2>Cookies</h2>
+            <p>Vi sparar all din data och säljer till högstbjudande.. bara så du vet</p>
+            <button @click="$emit('close')">Close</button>              
+        </slot>
+
+            <hr>
+        <slot name="links">
+            <a href="#">My website</a>
+            <a href="#">Google</a>
+            <a href="#">Altavista</a>
+        </slot>
+
+
+      </div>
+  </div>
+</teleport>
+</template>
+
+<script>
+export default {
+    name: 'ModalComponent'
+}
+</script>
+
+<style>
+.modal-wrapper {
+    background: #00000055;
+    position: absolute;
+    inset: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.modal{
+    background: #fff;
+    padding: 3rem;
+    border-radius: .5em;
+    box-shadow: 0 10px 2rem #11111177, 0 10px 5rem #33333381;
+}
+.input-group {
+    margin-bottom: 2rem;
+}
+.input-group label span {
+    display: block;
+    margin-bottom: 1rem;
+}
+.input-group label input {
+    display: block;
+    width: 100%;
+    border-radius: .5em;
+    border: 1px solid #333;
+}
+</style>
